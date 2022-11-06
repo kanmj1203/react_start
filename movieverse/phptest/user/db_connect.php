e2fb8c264a1da506127cb8fcf103d70794de7543
kanmj1203 <rkdalswl1203@g.shingu.ac.kr>
2022-11-07 00:40:54 +0900

    되돌리기용
    ㅁㅁ

--- movieverse/phptest/user/db_connect.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-	// DB 접속
-	$db = new PDO("mysql:host=uws7-028.cafe24.com;port=3306;dbname=movieverse", "movieverse", "1234");
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
